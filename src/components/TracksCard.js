77574af542b8544cb7cd0f9b5d656f86d62e1a13
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 01:16:53 +0530

    button animation

--- src/components/TracksCard.js
+++ src/components/TracksCard.js
@@ -8,11 +8,11 @@ const TracksCard = () => {
         <>
             {TracksData.map((Tracks, index) => (
                 <div key={index}>
-                    <div className='px-[10%] justify-around'>
+                    <div className='md:px-[10%]  justify-around'>
                         <div className='grid-rows-2 px-3'>
                             <div className='opacity-90 pb-[10%]'>
                                 <div className='flex px-2.5'>
-                                    <h1 className='font-bold text-4xl pb-[1%]'>{Tracks.TracksName}</h1>
+                                    <h1 className='font-bold text-4xl pb-[1%] justify-around '>{Tracks.TracksName}</h1>
                                 </div>
                                 <div className='pb-[1%]'>
                                     <p className='px-3 justify-around text-justify'>
@@ -21,7 +21,7 @@ const TracksCard = () => {
                                 </div>
                                 <div className='px-3 pb-[1%]'>
                                     <a href="/">
-                                        <button type = "button" className='button py-2 px-4 bg-gradient-to-tl from-blue-950 to-blue-500 text-white rounded-lg font-semibold'>Read More</button>
+                                        <button type = "button" className='button py-2 px-4 hover:scale-[1.1] bg-gradient-to-tl from-blue-950 to-blue-500 text-white rounded-lg font-semibold'>Read More</button>
                                     </a>
                                 </div>
                             </div>
